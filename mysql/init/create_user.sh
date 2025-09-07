8576c9c42e99c5a1c81aff7c3e785af688d77aea
pedrojaraujo <pedroa990@gmail.com>
2025-09-07 19:43:59 -0300

    atualiza create_user.sh: altera variáveis de ambiente para DB_USER e DB_PASSWORD

--- mysql/init/create_user.sh
+++ mysql/init/create_user.sh
@@ -4,14 +4,14 @@ set -e
 # Verifica se as variáveis de ambiente necessárias estão definidas
 : "${MYSQL_ROOT_PASSWORD:?Precisa definir MYSQL_ROOT_PASSWORD}"
 : "${MYSQL_DATABASE:?Precisa definir MYSQL_DATABASE}"
-: "${MYSQL_USER:?Precisa definir MYSQL_USER}"
-: "${MYSQL_PASSWORD:?Precisa definir MYSQL_PASSWORD}"
+: "${DB_USER:?Precisa definir DB_USER}"
+: "${DB_PASSWORD:?Precisa definir DB_PASSWORD}"
 
 echo "[Entrypoint] criando usuário de app..."
 
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
   CREATE DATABASE IF NOT EXISTS \\`$MYSQL_DATABASE\\`;
-  CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-  GRANT ALL PRIVILEGES ON \\`$MYSQL_DATABASE\\`.* TO '$MYSQL_USER'@'%';
+  CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+  GRANT ALL PRIVILEGES ON \\`$MYSQL_DATABASE\\`.* TO '$DB_USER'@'%';
   FLUSH PRIVILEGES;
 EOSQL
